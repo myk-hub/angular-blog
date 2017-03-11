7f5ccafd4be6c4fadf69f2149b0b2bbd3abe1dcd
usernamen7 <n7username7@gmail.com>
2017-03-11 13:08:15 +0200

    add controller

---
+++ src/js/app/blog-list/blog-list.component.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('blogList').
+component('blogList', {
+            template: "<div class=''><h2 class='new-class'>{{ title }} </h2><button ng-click='someClickTest()'>Click me</button></div>",
+            controller: function($scope) {
+                $scope.title = 'Hi there'
+                $scope.clicks = 0
+                $scope.someClickTest = function() {
+                    console.log('clicked')
+                    $scope.clicks += 1
+                    $scope.title = 'clicked ' + $scope.clicks + ' times'
+                }
+            }
+        });
+
+            // controller('BlogListController', function($scope) {
+            // console.log("Hello Michan")
+            // $scope.title = 'Hi there'
+            // $scope.clicks = 0
+            // $scope.someClickTest = function() {
+            //     console.log('clicked')
+            //     $scope.clicks += 1
+            //     $scope.title = 'clicked ' + $scope.clicks + ' times'
+            // }
+        // });
+
+        //component('blogList');
