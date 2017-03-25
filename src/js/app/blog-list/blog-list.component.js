f02f4cdcd14a8da68ace987815b524234faf589e
usernamen7 <n7username7@gmail.com>
2017-03-25 20:51:01 +0200

    Add rack server

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -5,6 +5,15 @@ component('blogList', {
             //template: "<div class=''><h2 class='new-class'>{{ title }} </h2><button ng-click='someClickTest()'>Click me</button></div>",
             templateUrl: 'templates/blog-list.html',
             controller: function($scope) {
+                var blogItems = [
+                    {title: "Some title", id: 1, description: "This is a book", publishDate: "25-03-2017"},
+                    {title: "Title", id: 2, description: "This is a book"},
+                    {title: "Tea", id: 3, description: "This is a book"},
+                    {title: "Lite", id: 4, description: "This is a book"},
+                ]
+
+                $scope.items = blogItems;
+
                 $scope.title = 'Hi there'
                 $scope.clicks = 0
                 $scope.someClickTest = function() {
