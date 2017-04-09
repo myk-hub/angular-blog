6cbfe462af12451deba4bc20e38f688f88383976
usernamen7 <n7username7@gmail.com>
2017-04-09 17:52:48 +0300

    mapping to a Restfull API

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,28 +3,21 @@
 angular.module('blogDetail').
 component('blogDetail', {
         templateUrl: 'templates/blog-detail.html',
-            controller: function($location, $routeParams, $scope) {
-              var blogItems = [
-                  {title: "Some title", id: 1, description: "This is a book", publishDate: "25-03-2017"},
-                  {title: "Title", id: 2, description: "This is a book"},
-                  {title: "Tea", id: 3, description: "This is a book"},
-                  {title: "Lite", id: 4, description: "This is a book"},
-              ]
-
-                //console.log($routeParams);
-                $scope.title = "Blog " + $routeParams.id
+            controller: function(Post, $http, $location, $routeParams, $scope) {
+              Post.query(function(data){
                 $scope.notFound = true
-                angular.forEach(blogItems, function(post) {
+                angular.forEach(data, function(post) {
                   if (post.id == $routeParams.id) {
                     $scope.notFound = false
                     $scope.post = post
                   }
                 })
+              })
 
-                if ($scope.notFound) {
-                  console.log("Not Found")
-                  //change location
-                  $location.path("/")
-                }
+              if ($scope.notFound) {
+                console.log("Not Found")
+                //change location
+                $location.path("/")
+              }
             }
         });
--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -2,39 +2,8 @@
 
 angular.module('blogList').
 component('blogList', {
-            //template: "<div class=''><h2 class='new-class'>{{ title }} </h2><button ng-click='someClickTest()'>Click me</button></div>",
             templateUrl: 'templates/blog-list.html',
-            controller: function($routeParams, $scope) {
-                console.log($routeParams);
-
-                var blogItems = [
-                    {title: "Some title", id: 1, description: "This is a book", publishDate: "25-03-2017"},
-                    {title: "Title", id: 2, description: "This is a book"},
-                    {title: "Tea", id: 3, description: "This is a book"},
-                    {title: "Lite", id: 4, description: "This is a book"},
-                ]
-
-                $scope.items = blogItems;
-
-                $scope.title = 'Hi there'
-                $scope.clicks = 0
-                $scope.someClickTest = function() {
-                    console.log('clicked')
-                    $scope.clicks += 1
-                    $scope.title = 'clicked ' + $scope.clicks + ' times'
-                }
+            controller: function(Post, $routeParams, $scope) {
+                $scope.items = Post.query();
             }
         });
-
-            // controller('BlogListController', function($scope) {
-            // console.log("Hello Michan")
-            // $scope.title = 'Hi there'
-            // $scope.clicks = 0
-            // $scope.someClickTest = function() {
-            //     console.log('clicked')
-            //     $scope.clicks += 1
-            //     $scope.title = 'clicked ' + $scope.clicks + ' times'
-            // }
-        // });
-
-        //component('blogList');
